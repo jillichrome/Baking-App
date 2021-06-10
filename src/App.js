57d0350ad6c3141ec3384dedd55bf5f3948424d8
jillichrome <jmfronk.17@gmail.com>
2021-06-10 11:59:14 -0400

    Add images

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import Header from './components/header.js';
 import BakedGood from './components/baked-good-card.js';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {}
+    };
+    this.getData = this.getData.bind(this);
+  }
+
+  getData() {
+    const info = require('./data.json');
+    this.setState({data: info});
+  }
+
+  componentDidMount(){
+    this.getData();
+  }
+
   render() {
     return(
       <div>
         <Header />
         <main>
-          <BakedGood />
+          <BakedGood data={this.state.data.cards} />
         </main>
       </div>
     )
